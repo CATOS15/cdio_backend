a1c1c7c768b20fcdea3c1a9666847bbb859423d9
Oliver <omoeltoft@gmail.com>
2021-02-15 00:16:53 +0100

    Test på port 5005 via service

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ def hello():
     return "Test commit"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    #Vi vil optimalt gerne knytte denne til port 80 
+    #men så skal vi have flettet det sammen med nginx 
+    #eller gøre så man har permission via service til at bind port 80
+    app.run(host='0.0.0.0', port=5005) 
