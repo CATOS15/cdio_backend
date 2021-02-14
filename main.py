0d310122e2635cc43ef22a2a2c804e2f429dbd40
Oliver <omoeltoft@gmail.com>
2021-02-15 00:03:29 +0100

    test commit for git på server

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app.config['SECRET_KEY'] = 'back3nd_!cdio'
 
 @app.route('/')
 def hello():
-    return "Test deployment backend"
+    return "Test commit"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000) #Port 80 i stedet? Tror den lige nu er låst af nginx og python skal også køres som en service på et tidspunkt
