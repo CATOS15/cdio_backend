caae5fe48287f9e85258453d3fdf54e548bd730f
Oliver <omoeltoft@gmail.com>
2021-02-14 23:50:40 +0100

    flask setup med ip/port

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'back3nd_!cdio'
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Test deployment backend"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000) #Port 80 i stedet? Tror den lige nu er låst af nginx og python skal også køres som en service på et tidspunkt
