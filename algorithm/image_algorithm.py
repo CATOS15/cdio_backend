63289eaed2c6d9a6f44027dcbd50eb78fc4d2b21
Oliver <omoeltoft@gmail.com>
2021-03-24 12:21:04 +0100

    rettelser til getmove delen af 7 kabale algoritmen

--- algorithm/image_algorithm.py
+++ algorithm/image_algorithm.py
@@ -18,9 +18,9 @@ class Fountain:
         self.suit = suit
 
 class Move:
-    def __init__(self, cardFrom, cardFromIndex, fromStack, toStack, description):
-        self.cardFrom = cardFrom
-        self.cardFromIndex = cardFromIndex
+    def __init__(self, fromCard, fromCardIndex, fromStack, toStack, description):
+        self.fromCard = fromCard
+        self.fromCardIndex = fromCardIndex
         self.fromStack = fromStack
         self.toStack = toStack
         self.description = description
@@ -35,7 +35,7 @@ fountains = [
 ]
 
 stacks = [
-    [Card(13, suit.SPADE), Card(12, suit.DIAMOND), Card(11, suit.CLUBS), Card(10, suit.DIAMOND)],
+    [Card(13, suit.SPADE), Card(12, suit.DIAMOND), Card(11, suit.CLUBS), Card(10, suit.DIAMOND), Card(9, suit.CLUBS)],
     [Card(13, suit.CLUBS)],
     [Card(11, suit.HEART)],
     [],
@@ -44,101 +44,86 @@ stacks = [
     [None, None, Card(10, suit.SPADE), Card(9, suit.HEART), Card(8, suit.CLUBS), Card(7, suit.HEART)]
 ]
 
-cardpile = [Card(9, suit.CLUBS)]
+cardpile = [Card(7, suit.DIAMOND)]
 
-def rule_move_to_stack(cardFrom, cardTo):
-    if cardFrom is None or cardTo is None:
+def rule_move_to_stack(fromCard, toStack):
+    if len(toStack) == 0:
+        if fromCard is not None and fromCard.number == 13:
+            return True
         return False
-    if cardFrom.number + 1 == cardTo.number and (((cardFrom.suit.value < 3 and cardTo.suit.value > 2) or (cardFrom.suit.value > 2 and cardTo.suit.value < 3)) or (cardTo.suit == None)):
+    toCard = toStack[len(toStack)-1]
+    if fromCard is None or toCard is None: #Hvis det er et facedown kort så skal den ikke med.
+        return False
+    if fromCard.number + 1 == toCard.number and ((fromCard.suit.value < 3 and toCard.suit.value > 2) or (fromCard.suit.value > 2 and toCard.suit.value < 3)):
         return True
     return False 
 
-def rule_move_to_fountain(stack, cardFrom, fountain):
-    if cardFrom is None:
-        return False
-    topcard = stack[len(stack)-1]
-    if (topcard.number == cardFrom.number and topcard.suit == cardFrom.suit):
-        if fountain.count + 1 == cardFrom.number and fountain.suit == cardFrom.suit:
-            return True
+def rule_move_to_fountain(fromCard, fountain):
+    if fromCard is not None and fromCard.number == fountain.count + 1 and fromCard.suit == fountain.suit:
+        return True
     return False
 
-def get_moves(stackMandatory1 = None, stackMandatory2 = None):
-    moves = []
+def append_legit_move(stackMandatory1, stackMandatory2, fromStack, fromStackIndex, toStack, toStackIndex, moves, fromCard, fromCardIndex, messageformat):
+    if stackMandatory1 == None or stackMandatory2 == None or fromStack == stackMandatory1 or fromStack == stackMandatory2 or toStack == stackMandatory1 or toStack == stackMandatory2:
+        if(messageformat == "stack_fountain"):
+            moves.append(Move(fromCard, fromCardIndex, fromStack, toStack, "Fra stack("+str(fromStackIndex+1)+") " + str(fromCard.suit) + "(" + str(fromCard.number) + ") til fountain " + str(fromCard.suit)))
+        elif(messageformat == "stack_stack"):
+            moves.append(Move(fromCard, fromCardIndex, fromStack, toStack, "Fra stack("+str(fromStackIndex+1)+") " + str(fromCard.suit) + "(" + str(fromCard.number) + ") til stack(" + str(toStackIndex+1) + ")"))
+        elif(messageformat == "cardpile_stack"):
+            moves.append(Move(fromCard, fromCardIndex, fromStack, toStack, "Fra cardpile " + str(fromCard.suit) + "(" + str(fromCard.number) + ") til stack(" + str(toStackIndex+1) + ")"))
+        elif(messageformat == "cardpile_fountain"):
+            moves.append(Move(fromCard, fromCardIndex, fromStack, toStack, "Fra cardpile " + str(fromCard.suit) + "(" + str(fromCard.number) + ") til fountain " + str(fromCard.suit)))
 
-    for fromIndex, fromStack in enumerate(stacks):
+# stackMandatory1 og stackMandatory2 er brugt efter man har foretaget sig et 'move'
+# Det er fordi at når man har foretaget sig et 'move' så er de næste 'moves' der skal udregnes kun ud fra de stacks man har benyttet til det første move
+def get_moves(stackMandatory1 = None, stackMandatory2 = None): 
+    moves = []
+    for fromStackIndex, fromStack in enumerate(stacks):
         if len(fromStack) == 0:
             continue
-        for toIndex, toStack in enumerate(stacks):
+        for toStackIndex, toStack in enumerate(stacks):
             if fromStack != toStack:
-                for cardIndex, card in enumerate(fromStack):
-                    if len(toStack) == 0:
-                        if card != None and card.number == 13:
-                            if stackMandatory1 == None or (fromStack == stackMandatory1 or toStack == stackMandatory1):
-                                moves.append(Move(card, cardIndex, fromStack, toStack, "Fra stack("+str(fromIndex+1)+") " + str(card.suit) + "(" + str(card.number) + ") til stack(" + str(toIndex+1) + ")"))
-                            elif stackMandatory2 == None or (fromStack == stackMandatory2 or toStack == stackMandatory2):
-                                moves.append(Move(card, cardIndex, fromStack, toStack, "Fra stack("+str(fromIndex+1)+") " + str(card.suit) + "(" + str(card.number) + ") til stack(" + str(toIndex+1) + ")"))
-                    elif rule_move_to_stack(card, toStack[len(toStack)-1]):
-                        if stackMandatory1 == None or (fromStack == stackMandatory1 or toStack == stackMandatory1):
-                            moves.append(Move(card, cardIndex, fromStack, toStack, "Fra stack("+str(fromIndex+1)+") " + str(card.suit) + "(" + str(card.number) + ") til stack(" + str(toIndex+1) + ")"))
-                        elif stackMandatory2 == None or (fromStack == stackMandatory2 or toStack == stackMandatory2):
-                            moves.append(Move(card, cardIndex, fromStack, toStack, "Fra stack("+str(fromIndex+1)+") " + str(card.suit) + "(" + str(card.number) + ") til stack(" + str(toIndex+1) + ")"))
-        for fountain in fountains:
-            cardFrom = fromStack[len(fromStack)-1]
-            if rule_move_to_fountain(fromStack, cardFrom, fountain):
-                if stackMandatory1 == None or (fromStack == stackMandatory1 or fountain == stackMandatory1):
-                    moves.append(Move(cardFrom, len(fromStack)-1, fromStack, fountain, "Fra stack("+str(fromIndex+1)+") " + str(cardFrom.suit) + "(" + str(cardFrom.number) + ") til fountain " + str(fountain.suit)))
-                elif stackMandatory2 == None or (fromStack == stackMandatory2 or fountain == stackMandatory2):
-                    moves.append(Move(cardFrom, len(fromStack)-1, fromStack, fountain, "Fra stack("+str(fromIndex+1)+") " + str(cardFrom.suit) + "(" + str(cardFrom.number) + ") til fountain " + str(fountain.suit)))
-    
+                for fromCardIndex, fromCard in enumerate(fromStack):
+                    if rule_move_to_stack(fromCard, toStack):
+                        append_legit_move(stackMandatory1, stackMandatory2, fromStack, fromStackIndex, toStack, toStackIndex, moves, fromCard, fromCardIndex, "stack_stack")
+        for fountainIndex, fountain in enumerate(fountains):
+            fromCard = fromStack[len(fromStack)-1]
+            if rule_move_to_fountain(fromCard, fountain):
+                append_legit_move(stackMandatory1, stackMandatory2, fromStack, fromStackIndex, fountain, fountainIndex, moves, fromCard, fromCardIndex, "stack_fountain")
     if len(cardpile) > 0:
         cardpile_topcard = cardpile[len(cardpile)-1]
-        for toIndex, toStack in enumerate(stacks):
-            if len(toStack) == 0:
-                if cardpile_topcard != None and cardpile_topcard.number == 13:
-                    if stackMandatory1 == None or (cardpile == stackMandatory1 or toStack == stackMandatory1):
-                        moves.append(Move(cardpile_topcard, len(cardpile)-1, cardpile, toStack, "Fra cardpile " + str(cardpile_topcard.suit) + "(" + str(cardpile_topcard.number) + ") til stack(" + str(toIndex+1) + ")"))
-                    elif stackMandatory2 == None or (cardpile == stackMandatory2 or toStack == stackMandatory2):
-                        moves.append(Move(cardpile_topcard, len(cardpile)-1, cardpile, toStack, "Fra cardpile " + str(cardpile_topcard.suit) + "(" + str(cardpile_topcard.number) + ") til stack(" + str(toIndex+1) + ")"))
-            elif rule_move_to_stack(cardpile_topcard, toStack[len(toStack)-1]):
-                if stackMandatory1 == None or (cardpile == stackMandatory1 or toStack == stackMandatory1):
-                    moves.append(Move(cardpile_topcard, len(cardpile)-1, cardpile, toStack, "Fra cardpile " + str(cardpile_topcard.suit) + "(" + str(cardpile_topcard.number) + ") til stack(" + str(toIndex+1) + ")"))
-                elif stackMandatory2 == None or (cardpile == stackMandatory2 or toStack == stackMandatory2):
-                    moves.append(Move(cardpile_topcard, len(cardpile)-1, cardpile, toStack, "Fra cardpile " + str(cardpile_topcard.suit) + "(" + str(cardpile_topcard.number) + ") til stack(" + str(toIndex+1) + ")"))
-        for fountain in fountains:
-            if rule_move_to_fountain(cardpile, cardpile_topcard, fountain):
-                if stackMandatory1 == None or (cardpile == stackMandatory1 or fountain == stackMandatory1):
-                    moves.append(Move(cardpile_topcard, len(cardpile)-1, cardpile, fountain, "Fra cardpile " + str(cardpile_topcard.suit) + "(" + str(cardpile_topcard.number) + ") til fountain " + str(fountain.suit)))
-                elif stackMandatory2 == None or (cardpile == stackMandatory2 or fountain == stackMandatory2):
-                    moves.append(Move(cardpile_topcard, len(cardpile)-1, cardpile, fountain, "Fra cardpile " + str(cardpile_topcard.suit) + "(" + str(cardpile_topcard.number) + ") til fountain " + str(fountain.suit)))
-
+        for toStackIndex, toStack in enumerate(stacks):
+            if rule_move_to_stack(cardpile_topcard, toStack):
+                append_legit_move(stackMandatory1, stackMandatory2, cardpile, -1, toStack, toStackIndex, moves, cardpile_topcard, len(cardpile)-1, "cardpile_stack")
+        for fountainIndex, fountain in enumerate(fountains):
+            if rule_move_to_fountain(cardpile_topcard, fountain):
+                append_legit_move(stackMandatory1, stackMandatory2, cardpile, -1, fountain, fountainIndex, moves, cardpile_topcard, len(cardpile)-1, "cardpile_fountain")
     return moves
-
-
 
 def _100_points(move):
     #Er det et ACE til fountain er det "bedste" move (Det kan ikke gøre ting værre)
-    if move.cardFrom.number == 1 and type(move.toStack) is Fountain:
+    if move.fromCard.number == 1 and type(move.toStack) is Fountain:
         return 100
     #Er det et TOER til fountain er det "bedste" move (Det kan ikke gøre ting værre)
-    if move.cardFrom.number == 2 and type(move.toStack) is Fountain:
+    if move.fromCard.number == 2 and type(move.toStack) is Fountain:
         return 100
     return 0
 
 def _95_points(move):
     #Ryk konge fra anden bunke til tom position for at åbne et NONE felt
-    if move.cardFromIndex > 0 and move.fromStack[move.cardFromIndex - 1] == None and move.cardFrom.number == 13 and len(move.toStack) == 0:
+    if move.fromCardIndex > 0 and move.fromStack[move.fromCardIndex - 1] == None and move.fromCard.number == 13 and len(move.toStack) == 0:
         return 95
     return 0
 
 def _90_points(move):
     #Ryk kort for at åbne et NONE felt
-    if move.cardFromIndex > 0 and move.fromStack[move.cardFromIndex - 1] == None:
+    if move.fromCardIndex > 0 and move.fromStack[move.fromCardIndex - 1] == None:
         return 90
     return 0
 
 def _75_points(move):
     #Ryk konge fra cardpile til tom position
-    if len(cardpile) > 0 and cardpile[len(cardpile)-1] == move.cardFrom and move.cardFrom.number == 13 and len(move.toStack) == 0:
+    if len(cardpile) > 0 and cardpile[len(cardpile)-1] == move.fromCard and move.fromCard.number == 13 and len(move.toStack) == 0:
         return 75
     return 0
 
@@ -150,24 +135,24 @@ def _50_points(move):
 
 def _45_points(move):
     #Rykke et kort fra cardpile til en stack på bordet
-    if len(cardpile) > 0 and cardpile[len(cardpile)-1] == move.cardFrom and type(move.toStack) is list:
+    if len(cardpile) > 0 and cardpile[len(cardpile)-1] == move.fromCard and type(move.toStack) is list:
         return 45
     return 0
 
 def _25_points(move):
     #Frier en plads så en konge kan rykke der hen (Det specielle her er at det kun er godt hvis der er en konge der kan rykkes til det tomme felt ellers er det rigtig dårligt)
-    if move.cardFromIndex == 0 and move.cardFrom.number != 13 and len(move.toStack) > 0:
+    if move.fromCardIndex == 0 and move.fromCard.number != 13 and len(move.toStack) > 0:
         return 25
     return 0
 
 def _10_points(move):
     #Alle ting der giver 10 point er næsten ubrugelige (Gør ingen forskel) - Undtagelsen er hvis du skal rykke kort til Fountain og nogle kort blokerer
-    if move.cardFromIndex > 0 and move.fromStack[move.cardFromIndex - 1] != None and type(move.toStack) is list:
+    if move.fromCardIndex > 0 and move.fromStack[move.fromCardIndex - 1] != None and type(move.toStack) is list:
         return 10
     return 0
 def _1_points(move):
     #Absolut ubrugelig. Ryk konge fra tomt felt til anden tomt felt
-    if move.cardFromIndex == 0 and move.cardFrom.number == 13 and len(move.toStack) == 0: 
+    if move.fromCardIndex == 0 and move.fromCard.number == 13 and len(move.toStack) == 0: 
         return 1
     return 0
 
@@ -181,6 +166,9 @@ def set_best_move(moves, currentPoint, doRecursive):
     localBestPoint = 0
 
     for move in moves:
+        if(move.description == 'Fra cardpile suit.CLUBS(9) til stack(1)'):
+            print("ky")
+
         point = _100_points(move)
         point = _95_points(move) if point == 0 else point
         point = _90_points(move) if point == 0 else point
@@ -195,9 +183,10 @@ def set_best_move(moves, currentPoint, doRecursive):
             localBestPoint = point + currentPoint
         
         if doRecursive:
-            newStacks = sim_bestmove(move)
+            newStacks = simulate_newStacks(move)
             tempBestPoint = set_best_move(get_moves(newStacks[0], newStacks[1]), point + currentPoint, False)
             localBestPoint = tempBestPoint if tempBestPoint != 0 else localBestPoint
+
 
             fountains = copy.copy(originalFountains)
             stacks = copy.copy(originalStacks)
@@ -212,38 +201,45 @@ def set_best_move(moves, currentPoint, doRecursive):
 
     return localBestPoint
 
-def sim_bestmove(bestMove):
+def simulate_newStacks(move):
     global cardpile, stacks, fountains
     cardMoved = None
 
     newStacks = [None, None]
 
-    if bestMove.fromStack == cardpile:
-        cardMoved = cardpile[bestMove.cardFromIndex:]
-        cardpile = cardpile[:bestMove.cardFromIndex]
+    if move.fromStack == cardpile:
+        cardMoved = cardpile[move.fromCardIndex:]
+        cardpile = cardpile[:move.fromCardIndex]
         newStacks[0] = cardpile
     else:
         for index, stack in enumerate(stacks):
-            if(stack == bestMove.fromStack):
-                cardMoved = stack[bestMove.cardFromIndex:]
-                stacks[index] = stack[:bestMove.cardFromIndex]
+            if(stack == move.fromStack):
+                cardMoved = stack[move.fromCardIndex:]
+                stacks[index] = stack[:move.fromCardIndex]
                 newStacks[0] = stacks[index]
         for index, fountain in enumerate(fountains):
-            if(fountain == bestMove.fromStack):
-                cardMoved = fountain[bestMove.cardFromIndex:]
-                fountains[index] = fountain[:bestMove.cardFromIndex]
+            if(fountain == move.fromStack):
+                cardMoved = fountain[move.fromCardIndex:]
+                fountains[index] = fountain[:move.fromCardIndex]
                 newStacks[0] = fountains[index]
 
     for index, stack in enumerate(stacks):
-        if(stack == bestMove.toStack and cardMoved != None):
-            stacks[index] = bestMove.toStack + cardMoved
+        if(stack == move.toStack and cardMoved != None):
+            stacks[index] = move.toStack + cardMoved
             newStacks[1] = stacks[index]
     for index, fountain in enumerate(fountains):
-        if(fountain == bestMove.toStack):
+        if(fountain == move.toStack):
             fountains[index].count = fountains[index].count + 1
             newStacks[1] = fountains[index]
 
     return newStacks
+
+
+
+
+
+
+
 
 originalFountains = copy.copy(fountains)
 originalStacks = copy.copy(stacks)
@@ -252,7 +248,8 @@ originalCardpile = copy.copy(cardpile)
 for x in get_moves():
     print(x.description)
 
-print("best move:")
+print("\n")
+print("Bedste træk:")
 set_best_move(get_moves(), 0, True)
 print(bestPoint)
 print(bestMove.description)
