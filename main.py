19fcf14b2948fb41910c4e1e8566f6927ee92652
creoludifico <omoeltoft@gmail.com>
2021-06-07 15:08:11 +0200

    api kald klar med appen nu

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'back3nd_!cdio'
-app.config["IMAGE_UPLOADS"] = "C:/Users/Nikolai/Desktop/Python/cdio_backend"
+app.config["IMAGE_UPLOADS"] = "images"
 # app.config["IMAGE_TEST"] = url("C:\Users\Nikolai\Desktop\Python\cdio_backend")
 CORS(app)
 
@@ -27,24 +27,96 @@ def test_endpoint2():
 
 @app.route('/upload', methods = ['POST'])
 def calculateImage():
-    imagefile = request.files.get('file', '')
+    imagefile = request.files.get('file')
     imagefile.save("image.jpg")
-    print(imagefile.filename)
 
-    dictionary ={   
-        "firstcard": "2d",   
-        "secondcard": "3c",   
-        "movemessage": "move your 2d to your 3c"
-    }   
-
-    json_object = json.dumps(dictionary)
+    json_object = json.dumps(getAlgData())
     return json_object
 
-@app.route("/algtest", methods = ['POST'])
+@app.route("/algtest")
 def algtest():
-    description = run_algorithm(request.get_json())
-    res = make_response(jsonify({'message': description}), 200)
-    return res
+    json_object = json.dumps(getAlgData())
+    return json_object
+
+
+def getAlgData():
+    dictionary = None
+
+    data_solitaire = {
+        'stacks': [
+            [
+                {'number': 4, 'suit': 3}
+            ],
+            [
+                {'number': 3, 'suit': 2}
+            ],
+            [
+                {'number': 2, 'suit': 3}
+            ],
+            [
+                {'number': 3, 'suit': 4}
+            ],
+            [
+                {'number': 3, 'suit': 1}
+            ],
+            [
+                {'number': 4, 'suit': 1}
+            ],
+            [
+                {'number': 5, 'suit': 1}
+            ],
+        ],
+        'fountains': [
+            {'number': 1, 'suit': 1},
+            {'number': 1, 'suit': 2},
+            {'number': 0, 'suit': 3},
+            {'number': 1, 'suit': 4}
+        ],
+        'cardpile': [
+            {'number': 8, 'suit': 2}
+        ],
+    }
+
+    move = run_algorithm(data_solitaire)
+    
+    if type(move) == str:
+        dictionary ={   
+            "firstcard": "",
+            "secondcard": "",   
+            "movemessage": str(move)
+        }
+    else:
+        firstcard = str(getCardCharFromNumber(move.fromCard.number)) + str(getColourCharFromNumber(move.fromCard.suit))
+        secondcard = str(getCardCharFromNumber(move.toCard.number)) + str(getColourCharFromNumber(move.toCard.suit))
+        dictionary ={   
+            "firstcard": firstcard,   
+            "secondcard": secondcard,   
+            "movemessage": str(move.description)
+        }
+
+    return dictionary
+
+def getCardCharFromNumber(number):
+    if number == 1:
+        return "a"
+    if number == 11:
+        return "j"
+    if number == 12:
+        return "q"
+    if number == 13:
+        return "k"
+    return number
+
+def getColourCharFromNumber(number):
+    if number.value == 1:
+        return "d"
+    if number.value == 2:
+        return "h"
+    if number.value == 3:
+        return "s"
+    if number.value == 4:
+        return "c"
+
 
 if __name__ == '__main__':
     #Vi vil optimalt gerne knytte denne til port 80 
