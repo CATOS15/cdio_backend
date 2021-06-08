7b17922e55c26fd7376e48db8a65d3f563814e27
creoludifico <omoeltoft@gmail.com>
2021-06-08 15:03:34 +0200

    opsætning af simulation for test af algoritme

--- algorithm/image_algorithm.py
+++ algorithm/image_algorithm.py
@@ -333,24 +333,28 @@ def run_algorithm(data_solitaire):
 
     bestMove = set_best_move()
 
-    for x in get_moves():
-        print(x.description)
+    #for x in get_moves():
+        #print(x.description)
 
-    print("\n")
-    print("Bedste træk:")
-    if bestMove != None:
-        print("Antal træk " + str(bestMove["numberOfMoves"]) + " giver " + str(bestMove["point"]) + " point")
-        print(bestMove["move"].description)
+    #print("\n")
+    #print("Bedste træk:")
+    #if bestMove != None:
+        #print("Antal træk " + str(bestMove["numberOfMoves"]) + " giver " + str(bestMove["point"]) + " point")
+        #print(bestMove["move"].description)
 
     if(bestMove != None):
+        bestMove["move"].drawCard = False
         if type(bestMove["move"].toStack) is Fountain:
             bestMove["move"].toCard = Card(bestMove["move"].toStack.count, bestMove["move"].toStack.suit)
         else:
             bestMove["move"].toCard = Card(bestMove["move"].toStack[len(bestMove["move"].toStack)-1].number, bestMove["move"].toStack[len(bestMove["move"].toStack)-1].suit)
 
         if (bestMove["point"] < 20 and bestMove["numberOfMoves"] == 1) or (bestMove["point"] < 40 and bestMove["numberOfMoves"] == 2) or len(originalCardpile) == 0:
-            bestMove["move"].description = "Flip bunken! Hvis ikke muligt udfør -> " + bestMove["move"].description
+            bestMove["move"].description = "Træk kort! Hvis ikke muligt udfør -> " + bestMove["move"].description
+            bestMove["move"].drawCard = True
     else:
-        bestMove["move"].description = "Flip bunken! Hvis ikke muligt så er der intet at gøre!"
+        bestMove = {"move": Move(None,None,None,None,"")}
+        bestMove["move"].description = "Træk kort"
+        bestMove["move"].drawCard = True
         
     return bestMove
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask.helpers import make_response
 from flask.json import jsonify
-from algorithm.image_algorithm import run_algorithm
+from algorithm.image_algorithm import Fountain, run_algorithm
 from flask import Flask, request
 from flask_cors import CORS
 import json
+import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'back3nd_!cdio'
@@ -25,8 +26,134 @@ def calculateImage():
 
 @app.route("/algtest")
 def algtest():
-    json_object = json.dumps(getAlgData())
+    json_object = json.dumps(run_simulation())
     return json_object
+
+waste = []
+def setupWaste():
+    global waste
+    for i in range(1, 14):  
+        waste.append({'number': i, 'suit': 1})
+        waste.append({'number': i, 'suit': 2})
+        waste.append({'number': i, 'suit': 3})
+        waste.append({'number': i, 'suit': 4})
+
+def drawCardFromWaste():
+    global waste
+
+    i = random.randint(0, len(waste)-1)
+    card = waste[i]
+    return card
+
+def getRandomCardFromWaste():
+    global waste
+
+    i = random.randint(0, len(waste)-1)
+    card = waste[i]
+    del waste[i]
+    return card
+
+def run_simulation():
+    global waste
+
+    firstcard = ""
+    secondcard = ""
+    movemessage = ""
+
+    state = 0
+
+    setupWaste()
+
+    data_solitaire = {
+        'stacks': [
+            [
+                getRandomCardFromWaste()
+            ],
+            [
+                getRandomCardFromWaste()
+            ],
+            [
+                getRandomCardFromWaste()
+            ],
+            [
+                getRandomCardFromWaste()
+            ],
+            [
+                getRandomCardFromWaste()
+            ],
+            [
+                getRandomCardFromWaste()
+            ],
+            [
+                getRandomCardFromWaste()
+            ],
+        ],
+        'fountains': [
+            {'number': 0, 'suit': 1},
+            {'number': 0, 'suit': 2},
+            {'number': 0, 'suit': 3},
+            {'number': 0, 'suit': 4}
+        ],
+        'cardpile': [
+            getRandomCardFromWaste()
+        ],
+    
+    }
+    print("\n")
+    print(data_solitaire)
+
+    bestMove = run_algorithm(data_solitaire)
+    #print(str(move.fromCard.number) + str (move.fromCard.suit.value))
+
+    cardsMoved = []
+
+    if bestMove["move"].drawCard:
+        if len(waste) == 0:
+            return {   
+                "firstcard": "new",   
+                "secondcard": "new",   
+                "movemessage": "Du har tabt!"
+            }
+        else:
+            data_solitaire['cardpile'][0] = drawCardFromWaste()
+    else:
+        if data_solitaire['cardpile'][0]["number"] == bestMove["move"].fromCard.number and data_solitaire['cardpile'][0]["suit"] == bestMove["move"].fromCard.suit.value:
+            cardsMoved = data_solitaire['cardpile']
+            data_solitaire['cardpile'][0] = getRandomCardFromWaste()
+        else:
+            for stackIndex, stack in enumerate(data_solitaire['stacks']):
+                for cardIndex, card in enumerate(stack):
+                    if card["number"] == bestMove["move"].fromCard.number and card["suit"] == bestMove["move"].fromCard.suit.value:
+                        cardsMoved = data_solitaire['stacks'][stackIndex][cardIndex:]
+                        data_solitaire['stacks'][stackIndex] = data_solitaire['stacks'][stackIndex][:cardIndex]
+
+        if type(bestMove["move"].toStack) is Fountain:
+            data_solitaire['fountains'][bestMove["move"].toCard.suit.value-1]["number"] = bestMove["move"].toCard.number
+        else:
+            for stackIndex, stack in enumerate(data_solitaire['stacks']):
+                for cardIndex, card in enumerate(stack):
+                    if card["number"] == bestMove["move"].toCard.number and card["suit"] == bestMove["move"].toCard.suit.value:
+                        data_solitaire['stacks'][stackIndex] = data_solitaire['stacks'][stackIndex] + cardsMoved
+
+    print("\n")
+    print(data_solitaire)
+
+    if bestMove["move"].fromCard is not None and bestMove["move"].toCard is not None:
+        firstcard = str(getCardCharFromNumber(bestMove["move"].fromCard.number)) + str(getColourCharFromNumber(bestMove["move"].fromCard.suit))
+        secondcard = str(getCardCharFromNumber(bestMove["move"].toCard.number)) + str(getColourCharFromNumber(bestMove["move"].toCard.suit))
+        movemessage = str(bestMove["move"].description)
+
+        if (bestMove["point"] < 20 and bestMove["numberOfMoves"] == 1) or (bestMove["point"] < 40 and bestMove["numberOfMoves"] == 2):
+            firstcard = "flip"
+            secondcard = "random"
+    else:
+        movemessage = str(bestMove["move"].description)
+
+    return {   
+        "firstcard": firstcard,   
+        "secondcard": secondcard,   
+        "movemessage": movemessage
+    }
 
 
 def getAlgData():
@@ -79,7 +206,7 @@ def getAlgData():
         secondcard = str(getCardCharFromNumber(move.toCard.number)) + str(getColourCharFromNumber(move.toCard.suit))
         movemessage = str(move.description)
 
-        if (bestMove["point"] < 20 and bestMove["numberOfMoves"] == 1) or (bestMove["point"] < 40 and bestMove["numberOfMoves"] == 2) or len(originalCardpile) == 0:
+        if (bestMove["point"] < 20 and bestMove["numberOfMoves"] == 1) or (bestMove["point"] < 40 and bestMove["numberOfMoves"] == 2):
             firstcard = "flip"
             secondcard = "random"
     else:
