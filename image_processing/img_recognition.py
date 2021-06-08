e8050b9478416ca0aa2ab1a6766b5f9886488413
fluffems <mkl@mikiaholding.dk>
2021-06-08 13:10:11 +0200

    changed alternative path to absolute path NB! Put path to (incl) cdio_backend in pythonpath

--- image_processing/img_recognition.py
+++ image_processing/img_recognition.py
@@ -1,13 +1,17 @@
 from numpy.core.fromnumeric import resize
-from g_img import *
-from g_shared import *
-from img_wash import *
-from img_compare import *
-from img_resolution import *
-from debugging import *
-from img_cut_suit_rank import *
-# import os
-from pathlib import Path
+# from image_processing import objects
+import image_processing.objects as objects
+import image_processing.g_img as g_img
+import image_processing.g_shared as g_shared
+import image_processing.objects as objects
+import image_processing.img_wash as wash
+import image_processing.img_contour as contour
+import image_processing.img_compare as compare
+import image_processing.img_resolution as resolution
+import image_processing.debugging as debugging
+import image_processing.img_cut_suit_rank as cut_suit_rank
+import cv2
+# from pathlib import Path
 
 # Paths for images received
 path_card_tableau = 'image_processing/templates/test/full_deck/tableau.png'
@@ -41,8 +45,8 @@ test = card_waste_color
 # cut appropriately suit and number
 # recognize number
 # recognize suit
-flow_waste = Flow(cb_wash=otsu_wash, cb_contour=contour_approximation,
-                  cb_cut_suit_rank=find_by_hierachy, cb_compare_by_template=compare_ranksuit)
+flow_waste = objects.Flow(cb_wash=wash.otsu_wash, cb_contour=contour.contour_approximation,
+                  cb_cut_suit_rank=cut_suit_rank.find_by_hierachy, cb_compare_by_template=compare.compare_ranksuit)
 # find card outlines
 flow_waste_washed = flow_waste.execute_wash(test, cv2.THRESH_BINARY)
 
@@ -55,23 +59,23 @@ washed_images = []
 for i, cunt in enumerate(flow_waste_countours):
     washed_images.append(flow_waste.execute_wash(cunt, cv2.THRESH_BINARY))
 
-print_results(washed_images, path_contours_sp2)
+debugging.print_results(washed_images, g_shared.path_contours_sp2)
 
 # cut out images
 flow_waste_cut = flow_waste.execute_cut_suit_rank(
     washed_images, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-print_waste_cuts(flow_waste_cut)
+debugging.print_waste_cuts(flow_waste_cut)
 
 # set to black/white and invert templates
-bin_invert_templates(g_templates)
+resolution.bin_invert_templates(g_img.g_templates)
 
 # uses all cards
 # compares all contours of a card with all templates
 # creates a card from best match & global threshold
 results = []
 for i, card in enumerate(flow_waste_cut):
-    results.append(flow_waste.execute_compare_by_template(card, g_templates))
+    results.append(flow_waste.execute_compare_by_template(card, g_img.g_templates))
 
 
 for x in results:
---
+++ image_processing/tests/wash.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TestWash(unittest.TestCase):
+    
+    
+    
+    def test_quick(self):
+        self.assertEqual(sum([2,3]), 5, "6 is nice")
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
