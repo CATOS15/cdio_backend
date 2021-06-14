961b150660e4f16ee2fd7fb04578d27536c15c03
creoludifico <omoeltoft@gmail.com>
2021-06-14 15:18:32 +0200

    liveshare
    Co-authored-by: Vialian <Vialian@users.noreply.github.com>

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ CORS(app)
 def hello():
     return "CDIO API"
 
-
 @app.route('/upload', methods=['POST'])
 def calculateImage():
     imagefile = request.files.get('file')
@@ -29,341 +28,36 @@ def calculateImage():
 @app.route("/algtest", methods = ['POST'])
 def algtestpost():
     bestMove = run_algorithm(request.get_json())
-    res = make_response(jsonify({'message': bestMove["move"].description}), 200)
-    return res
+    if "won" in bestMove and bestMove["won"]:
+        res = make_response(jsonify({'message': "VANDT!", "firstcard": "won", "secondcard": "won", "movemessage": "", 'location': ""}), 200)
+        return res
 
-@app.route("/algtest")
-def algtestget():
-    json_object = json.dumps(run_simulation())
-    return json_object
-
-waste = []
-noneCards = []
-def setupWaste():
-    global waste
-    for i in range(1, 14):  
-        waste.append({'number': i, 'suit': 1})
-        waste.append({'number': i, 'suit': 2})
-        waste.append({'number': i, 'suit': 3})
-        waste.append({'number': i, 'suit': 4})
-    random.shuffle(waste)
-
-def setupNoneCards():
-    global waste, noneCards
-    for i in range(21):  
-        noneCards.append(drawCard())
-
-def drawNoneCard():
-    global noneCards
-
-    i = 0
-    card = noneCards[i]
-    del noneCards[i]
-    
-    return card
-
-def toWaste(card):
-    global waste
-    waste.append(card)
-
-def drawCard():
-    global waste
-
-    i = 0
-    card = waste[i]
-    del waste[i]
-    
-    return card
-
-def run_simulation():
-    global waste
+    move = bestMove["move"]
 
     firstcard = ""
     secondcard = ""
     movemessage = ""
 
-    setupWaste()
-    setupNoneCards()
-
-    data_solitaire = {
-        'stacks': [
-            [
-                drawCard()
-            ],
-            [
-                None,
-                drawCard()
-            ],
-            [
-                None,
-                None,
-                drawCard()
-            ],
-            [
-                None,
-                None,
-                None,
-                drawCard()
-            ],
-            [
-                None,
-                None,
-                None,
-                None,
-                drawCard()
-            ],
-            [
-                None,
-                None,
-                None,
-                None,
-                None,
-                drawCard()
-            ],
-            [
-                None,
-                None,
-                None,
-                None,
-                None,
-                None,
-                drawCard()
-            ],
-        ],
-        'fountains': [
-            {'number': 0, 'suit': 1},
-            {'number': 0, 'suit': 2},
-            {'number': 0, 'suit': 3},
-            {'number': 0, 'suit': 4}
-        ],
-        'cardpile': [
-            drawCard()
-        ],
-    }
-
-    # data_solitaire = {
-    #     "stacks":[
-    #         [
-    #             {
-    #                 "number":5,
-    #                 "suit":2
-    #             },
-    #             {
-    #                 "number":4,
-    #                 "suit":4
-    #             }
-    #         ],
-    #         [
-                
-    #         ],
-    #         [
-    #             None,
-    #             None,
-    #             {
-    #                 "number":11,
-    #                 "suit":1
-    #             },
-    #             {
-    #                 "number":10,
-    #                 "suit":4
-    #             },
-    #             {
-    #                 "number":9,
-    #                 "suit":1
-    #             },
-    #             {
-    #                 "number":8,
-    #                 "suit":4
-    #             },
-    #             {
-    #                 "number":7,
-    #                 "suit":1
-    #             },
-    #             {
-    #                 "number":6,
-    #                 "suit":4
-    #             },
-    #             {
-    #                 "number":5,
-    #                 "suit":1
-    #             }
-    #         ],
-    #         [
-    #             {
-    #                 "number":11,
-    #                 "suit":4
-    #             },
-    #             {
-    #                 "number":10,
-    #                 "suit":1
-    #             },
-    #             {
-    #                 "number":9,
-    #                 "suit":3
-    #             },
-    #             {
-    #                 "number":8,
-    #                 "suit":2
-    #             },
-    #             {
-    #                 "number":7,
-    #                 "suit":3
-    #             },
-    #             {
-    #                 "number":9,
-    #                 "suit":3
-    #             }
-    #         ],
-    #         [
-    #             None,
-    #             {
-    #                 "number":4,
-    #                 "suit":3
-    #             }
-    #         ],
-    #         [
-    #             None,
-    #             None,
-    #             None,
-    #             None,
-    #             {
-    #                 "number":12,
-    #                 "suit":1
-    #             },
-    #             {
-    #                 "number":11,
-    #                 "suit":3
-    #             }
-    #         ],
-    #         [
-    #             None,
-    #             None,
-    #             None,
-    #             {
-    #                 "number":5,
-    #                 "suit":3
-    #             },
-    #             {
-    #                 "number":4,
-    #                 "suit":2
-    #             },
-    #             {
-    #                 "number":3,
-    #                 "suit":3
-    #             }
-    #         ]
-    #     ],
-    #     "fountains":[
-    #         {
-    #             "number":4,
-    #             "suit":1
-    #         },
-    #         {
-    #             "number":2,
-    #             "suit":2
-    #         },
-    #         {
-    #             "number":0,
-    #             "suit":3
-    #         },
-    #         {
-    #             "number":2,
-    #             "suit":4
-    #         }
-    #     ],
-    #     "cardpile":[
-    #         {
-    #             "number":7,
-    #             "suit":2
-    #         }
-    #     ]
-    #     }
-    print("\n")
-    print(data_solitaire)
-
-    for i in range(1, 200):
-
-        for stackIndex, stack in enumerate(data_solitaire['stacks']):
-            if len(stack) > 0 and stack[len(stack)-1] is None:
-                data_solitaire['stacks'][stackIndex][len(stack)-1] = drawNoneCard()
-
-
-        bestMove = run_algorithm(data_solitaire)
-        #print(str(move.fromCard.number) + str (move.fromCard.suit.value))
-
-        cardsMoved = []
-
-        if data_solitaire['fountains'][0]["number"] == 13 and data_solitaire['fountains'][1]["number"] == 13 and data_solitaire['fountains'][2]["number"] == 13 and data_solitaire['fountains'][3]["number"] == 13:
-            return {   
-                "firstcard": "new",   
-                "secondcard": "new",   
-                "movemessage": "Du har vundet!"
-            }
-
-        if bestMove["move"].drawCard:
-            if len(waste) == 0:
-                return {   
-                    "firstcard": "new",
-                    "secondcard": "new",
-                    "movemessage": "Du har tabt!"
-                }
-            else:
-                if data_solitaire['cardpile'][0] is not None:
-                    toWaste(data_solitaire['cardpile'][0])
-                    data_solitaire['cardpile'][0] = drawCard()
+    if type(move) != str and move.fromCard is not None:
+        firstcard = str(getCardCharFromNumberTest(move.fromCard.number)) + " " + str(getColourCharFromNumberTest(move.fromCard.suit))
+        if move.toCard is not None:
+            secondcard = str(getCardCharFromNumberTest(move.toCard.number)) + " " + str(getColourCharFromNumberTest(move.toCard.suit))
         else:
-            if data_solitaire['cardpile'][0]["number"] == bestMove["move"].fromCard.number and data_solitaire['cardpile'][0]["suit"] == bestMove["move"].fromCard.suit.value:
-                cardsMoved = data_solitaire['cardpile'].copy()
-                if len(waste) != 0:
-                    data_solitaire['cardpile'][0] = drawCard()
-            else:
-                for stackIndex, stack in enumerate(data_solitaire['stacks']):
-                    for cardIndex, card in enumerate(stack):
-                        if card is not None and card["number"] == bestMove["move"].fromCard.number and card["suit"] == bestMove["move"].fromCard.suit.value:
-                            cardsMoved = data_solitaire['stacks'][stackIndex][cardIndex:]
-                            data_solitaire['stacks'][stackIndex] = data_solitaire['stacks'][stackIndex][:cardIndex]
-
-            if type(bestMove["move"].toStack) is Fountain:
-                data_solitaire['fountains'][bestMove["move"].toCard.suit.value-1]["number"] += 1
-            else:
-                for stackIndex, stack in enumerate(data_solitaire['stacks']):
-                    for cardIndex, card in enumerate(stack):
-                        if card is not None:
-                            if len(bestMove["move"].toStack) == 0:
-                                if len(stack) == 0:
-                                    data_solitaire['stacks'][stackIndex] = data_solitaire['stacks'][stackIndex] + cardsMoved
-                            elif card["number"] == bestMove["move"].toCard.number and card["suit"] == bestMove["move"].toCard.suit.value:
-                                data_solitaire['stacks'][stackIndex] = data_solitaire['stacks'][stackIndex] + cardsMoved
-        
-        print(data_solitaire['cardpile'][0])
-        print(bestMove['move'].description)
-        print("\n")
-
-
-    print("\n")
-    print(data_solitaire)
-    print("\n")
-    print(waste)
-    print("\n")
-    print(bestMove)
-
-    if bestMove["move"].fromCard is not None and bestMove["move"].toCard is not None:
-        firstcard = str(getCardCharFromNumber(bestMove["move"].fromCard.number)) + str(getColourCharFromNumber(bestMove["move"].fromCard.suit))
-        secondcard = str(getCardCharFromNumber(bestMove["move"].toCard.number)) + str(getColourCharFromNumber(bestMove["move"].toCard.suit))
-        movemessage = str(bestMove["move"].description)
+            secondcard = "empty"
+        movemessage = str(move.description)
 
         if (bestMove["point"] < 20 and bestMove["numberOfMoves"] == 1) or (bestMove["point"] < 40 and bestMove["numberOfMoves"] == 2):
             firstcard = "flip"
             secondcard = "random"
     else:
-        movemessage = str(bestMove["move"].description)
+        movemessage = str(move)
 
-    return {   
-        "firstcard": firstcard,   
-        "secondcard": secondcard,   
-        "movemessage": movemessage
-    }
+    location = "stack"
+    if move and type(move.toStack) is Fountain:
+        location = "fountain"
 
+    res = make_response(jsonify({'message': bestMove["move"].description, "firstcard": firstcard, "secondcard": secondcard, "movemessage": movemessage, 'location': location}), 200)
+    return res
 
 def getAlgData():
     data_solitaire = {
@@ -439,6 +133,16 @@ def getCardCharFromNumber(number):
         return "k"
     return number
 
+def getCardCharFromNumberTest(number):
+    if number == 1:
+        return "A"
+    if number == 11:
+        return "J"
+    if number == 12:
+        return "Q"
+    if number == 13:
+        return "K"
+    return number
 
 def getColourCharFromNumber(number):
     if number.value == 1:
@@ -449,6 +153,17 @@ def getColourCharFromNumber(number):
         return "s"
     if number.value == 4:
         return "c"
+
+
+def getColourCharFromNumberTest(number):
+    if number.value == 1:
+        return "diamond"
+    if number.value == 2:
+        return "heart"
+    if number.value == 3:
+        return "spade"
+    if number.value == 4:
+        return "club"
 
 
 if __name__ == '__main__':
