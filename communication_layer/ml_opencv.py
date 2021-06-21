686783b67382043d3eca531f0daab3a4700e5f13
fluffems <mkl@mikiaholding.dk>
2021-06-21 18:04:11 +0200

    wip test

--- communication_layer/ml_opencv.py
+++ communication_layer/ml_opencv.py
@@ -2,7 +2,7 @@ import communication_layer.ml_alg as comm
 
 def compare_results(cv_results, ml_results):
     solitaire_alg = {}
-    solitaire_alg['waste'] = comm.map_opencv_alg(ml_results[0], comm.ImageCardType.Waste)
+    solitaire_alg['waste'] = comm.map_opencv_alg(cv_results[0], comm.ImageCardType.Waste)
     solitaire_alg['fountains'] = comm.map_opencv_alg(ml_results[1], comm.ImageCardType.Foundation)
     solitaire_alg['tableau'] = comm.map_opencv_alg(ml_results[2], comm.ImageCardType.Tableau)
     return solitaire_alg
