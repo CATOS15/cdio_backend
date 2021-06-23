a7dc9855b032c6facf49b2d523fb78e19595c766
Bircck <s195458@student.dtu.dk>
2021-06-23 20:33:43 +0200

    Sørget for index.html og algoritme test program virker

--- main.py
+++ main.py
@@ -36,12 +36,83 @@ def calculate_solution():
     bestmove = alg.run_algorithm(solitaire_alg)
     return app_comm.convert_alg_to_app_json(bestmove)
     
+#udelukkende til test af index.html skal ikke ændres!
 @app.route("/algtest", methods = ['POST'])
 def algtestpost():
-    res = alg.simple_test()
+    bestMove = run_algorithm(request.get_json())
+    if "won" in bestMove and bestMove["won"]:
+        res = make_response(jsonify({'message': "VANDT!", "firstcard": "won", "secondcard": "won", "movemessage": "", 'location': ""}), 200)
+        return res
+
+    move = bestMove["move"]
+
+    firstcard = ""
+    secondcard = ""
+    movemessage = ""
+
+    if type(move) != str and move.fromCard is not None:
+        firstcard = str(getCardCharFromNumberTest(move.fromCard.number)) + " " + str(getColourCharFromNumberTest(move.fromCard.suit))
+        if move.toCard is not None:
+            secondcard = str(getCardCharFromNumberTest(move.toCard.number)) + " " + str(getColourCharFromNumberTest(move.toCard.suit))
+        else:
+            secondcard = "empty"
+        movemessage = str(move.description)
+
+        if (bestMove["point"] < 20 and bestMove["numberOfMoves"] == 1) or (bestMove["point"] < 40 and bestMove["numberOfMoves"] == 2):
+            firstcard = "flip"
+            secondcard = "random"
+    else:
+        movemessage = str(move)
+
+    location = "tableau"
+    if move and type(move.toStack) is Fountain:
+        location = "fountain"
+
+    res = make_response(jsonify({'message': bestMove["move"].description, "firstcard": firstcard, "secondcard": secondcard, "movemessage": movemessage, 'location': location}), 200)
     return res
 
+def getCardCharFromNumber(number):
+    if number == 1:
+        return "a"
+    if number == 11:
+        return "j"
+    if number == 12:
+        return "q"
+    if number == 13:
+        return "k"
+    return number
 
+def getCardCharFromNumberTest(number):
+    if number == 1:
+        return "A"
+    if number == 11:
+        return "J"
+    if number == 12:
+        return "Q"
+    if number == 13:
+        return "K"
+    return number
+
+def getColourCharFromNumber(number):
+    if number.value == 1:
+        return "d"
+    if number.value == 2:
+        return "h"
+    if number.value == 3:
+        return "s"
+    if number.value == 4:
+        return "c"
+
+
+def getColourCharFromNumberTest(number):
+    if number.value == 1:
+        return "diamond"
+    if number.value == 2:
+        return "heart"
+    if number.value == 3:
+        return "spade"
+    if number.value == 4:
+        return "club"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5005)
